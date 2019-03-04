abd246b9c2a7e4ea3da5ef454ce5899dfb0246cd
wilfonba <wilfonba@rose-hulman.edu>
2019-03-04 16:03:09 -0500

    message

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -39,17 +39,17 @@ window = rg.TurtleWindow()
 snowballs = 3                                                  #
 ################################################################
 
-#initialize t1
+# initialize t1
 t1 = rg.SimpleTurtle('circle')
 t1.pen = rg.Pen('black', 3)
 t1.speed = 20
 r = int((100/3)*snowballs)
 c = int((-200/3)*snowballs)
 
-#create body of snowman
+# create body of snowman
 for k in range(snowballs):
     if k == 0:
-        #drawing the first snowball
+        # drawing the first snowball
         t1.pen_up()
         t1.go_to(rg.Point(0,c))
         t1.pen_down()
@@ -57,15 +57,15 @@ for k in range(snowballs):
         r = r - 20
         c = c + 2*r
     if k > 0:
-        #calculating start and end angles of each arc for
-        #snowballs after the first one
+        # calculating start and end angles of each arc for
+        # snowballs after the first one
         R = r + 20
         D = 2*r - 20
         d = ((D**2 + r**2 - R**2)/(2*D))
         theta = math.degrees(math.acos(d/r))
         print(r, R, D, d, theta)
 
-        #drawing the snowballs
+        # drawing the snowballs
         t1.pen_up()
         t1.go_to(rg.Point(0,c))
         t1.draw_Arc(r, theta)
@@ -77,27 +77,27 @@ for k in range(snowballs):
         c = c + 2*r
 
 r = r + 20
-#use t1 as right eye of snowman
+# use t1 as right eye of snowman
 t1.pen_up()
-t1.go_to(rg.Point((r)/2 , c))
+t1.go_to(rg.Point(r/2 , c))
 
-#redefine initials to draw buttons
+# redefine initials to draw buttons
 c2 = (-200/3)*snowballs
 r2 = (100/3)*snowballs
 
-#initialize t2
+# initialize t2
 t2 = rg.SimpleTurtle('circle')
 t2.pen = rg.Pen('black', 2)
 t2.speed = 20
 rb = r2/15
 
-#draw buttons on snowman
+# draw buttons on snowman
 buttons = 6
 
 if snowballs <= 2:
     buttons = 3
 
-#draw buttons
+# draw buttons
 for d in range(buttons):
     t2.pen_up()
     t2.go_to(rg.Point(0,c2/2))
@@ -109,12 +109,12 @@ for d in range(buttons):
 t2.pen_up()
 t2.go_to(rg.Point(-r/2, c))
 
-#initialize t3
+# initialize t3
 t3 = rg.SimpleTurtle('triangle')
 t3.pen_up()
 t3.speed = 20
 
-#make nose using t3
+# make nose using t3
 t3.go_to(rg.Point(0, c - r/2))
 
 window.close_on_mouse_click()
